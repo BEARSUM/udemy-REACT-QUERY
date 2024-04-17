e3cf6804dfbccf93a49937ad1a16d9fa5b006b69
[SW5] 문수민 <sum95771@gmail.com>
2024-04-18 02:15:43 +0900

    feat: staleTime 설정

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -13,6 +13,7 @@ export function Posts() {
   const { data, isError, isLoading } = useQuery({
     queryKey: ["posts"],
     queryFn: fetchPosts,
+    staleTime: 2000,
   });
   if (isLoading) {
     return <h3>Loading...</h3>;
